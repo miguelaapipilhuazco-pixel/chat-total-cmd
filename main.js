94f4d82567de7509eb9e6625cbb06fd2544d6565
MiguelAngelArriojaApipilhuazco <miguela.apipilhuazco@gmail.com>
2026-07-19 11:28:36 -0600

    Actualizacion del orquestador universal Multi-IA

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { app, BrowserWindow } from 'electron';
 function createOverlayWindow () {
   const win = new BrowserWindow({
-    width: 200, height: 260,
+    width: 300, height: 380,
     transparent: true, frame: false,
     alwaysOnTop: true, resizable: false, hasShadow: false,
     webPreferences: { nodeIntegration: false }
